4b67ad6d15a38b5af9cfab32305a91f0fb2b2d9e
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-10 05:55:26 +0000

    Added route for api. Two main routes, 1. app (path: /) 2. api (path: /api/todos). Consider adding second main path for api to seperate requests for all todos from requests for a single todo

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var express = require('express'),
 	app = express(),
 	port = 3000;
 
+var todoRoutes = require('./routes/todo')
+
 app.get('/', function (req, res){
-	res.json({"greeting":"Hello world!"})
+	res.send('Root route')
 })
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, function(){
 	console.log("Express server is running on port " + port)
