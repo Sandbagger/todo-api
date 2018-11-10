0b70b91f14c6c8e28f9d7f66f3e6019bcc22485f
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-10 05:29:28 +0000

    Define db schema and model

---
+++ models/todo.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var todoSchema = new mongoose.Schema({
+    name: {String,
+    required: 'Name is a required field'
+    },
+    completed: {
+        type: Boolean,
+        default: false, // only need to update field if true
+    },
+    entry_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
