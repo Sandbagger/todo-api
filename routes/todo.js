4b0e44f8acfbce356c19d8e36fbf8e39f2d3df95
Sandbagger <wneal_19@yahoo.co.uk>
2018-11-10 07:26:27 +0000

    Query db in api route. Expected empty array, and it returns an empty array

--- routes/todo.js
+++ routes/todo.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
 
 router.get('/', function(req, res){
-    res.send('Todo route')
+    db.Todo.find()
+    .then(function(todos){
+        res.json(todos)
+    }).catch(function(err){
+        res.send(err);
+    })
 })
 
 module.exports = router;
